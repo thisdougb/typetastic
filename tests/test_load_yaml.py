a18189f0787a6f5f4a370d9212b40a12fcad126c
Doug Bridgens <thisdougb@users.noreply.github.com>
2020-04-17 20:58:27 +0100

    tests for load_file with valid and invalid data.

---
+++ tests/test_load_yaml.py
@@ -0,0 +1,24 @@
+"""Test Load Commands."""
+
+import unittest
+import typetastic
+
+
+class TestLoadFile(unittest.TestCase):
+    """Test Load Command File."""
+
+    def test_load_valid_yaml_file(self):
+        """Test loading a valid yaml file."""
+
+        test_file = "tests/data/valid_file.yaml"
+        result = typetastic.Robot.load_file(test_file)
+
+        self.assertTrue(result)
+
+    def test_load_invalid_yaml_file(self):
+        """Test loading an invalid yaml file."""
+
+        test_file = "tests/data/invalid_file.yaml"
+        result = typetastic.Robot.load_file(test_file)
+
+        self.assertFalse(result)
--- typetastic/robot.py
+++ typetastic/robot.py
@@ -1,13 +1,28 @@
 """TypeTastic"""
 
+import yaml
+
 
 class Robot:
     """Robot that runs the commands."""
 
     def __init__(self):
-        Robot().hello()
+        pass
 
     @staticmethod
-    def hello():
-        """Basic test method."""
-        print("Hello World!")
+    def load_file(inputfile):
+        """Load YML file.
+
+        Returns:
+        A dictionary containing the data.
+        False if data could not be loaded.
+        """
+        with open(inputfile, 'r') as stream:
+            try:
+                print(stream)
+                commands = yaml.load(stream, Loader=yaml.FullLoader)
+                return commands
+
+            except yaml.YAMLError as error:
+                print(error)
+                return False
